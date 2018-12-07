b6d56fece340a6bb3eb49473215592cb3a2938c1
bhaveshc20 <bhaveshc789@gmail.com>
2018-12-06 20:12:48 -0800

    switched to axios, added custom marker and custom modal card

--- Home.js
+++ Home.js
@@ -1,21 +1,25 @@
 import React from 'react';
-import { StyleSheet, Text, View, Platform, Alert, Dimensions } from 'react-native';
+import { StyleSheet, Text, View, Platform, Alert, Dimensions, TouchableOpacity } from 'react-native';
 
 import MapView from 'react-native-maps';
-
-import { Constants, Location, Permissions } from 'expo';
+import { Constants, Location, Permissions, LinearGradient } from 'expo';
+import { Icon } from 'react-native-elements'
+import axios from 'axios';
 
 let { width, height } = Dimensions.get('window');
 const ASPECT_RATIO = width / height;
 const LATITUDE_DELTA = 0.0922;
 const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
 
+const CARD_HEIGHT = height / 12;
 
 export default class Home extends React.Component {
   state = {
     stations: null,
-    errorMessage: null,
-    region: {}
+    statInfo: {},
+    isOpen: false,
+    region: {},
+    coords: {},
   };
 
   _getLocationAsync = async () => {
@@ -51,61 +55,66 @@ export default class Home extends React.Component {
     this.fetchPlaces();
   }
 
-  async fetchPlaces() {
-    try {
-      let response = await fetch(`https://api.voltaapi.com/v1/stations`, {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
-        },
-      });
 
-      let responseJSON = null
-
-      if (response.status === 200) {
-        responseJSON = await response.json();
-
-
-        this.setState({ stations: responseJSON });
-      } else {
-        responseJSON = await response.json();
-        const error = responseJSON.message
-
-        // this.setState({ errors: responseJSON.errors })
-        Alert.alert('failed!', `Unable to load profile.. ${error}!`)
-      }
-    } catch (error) {
-      this.setState({ response: error })
-
-      console.log(error)
-
-      Alert.alert('failed ', 'Please try again later')
-    }
+  fetchPlaces() {
+    axios.get(`https://api.voltaapi.com/v1/stations`)
+      .then(res => {
+        const stations = res.data;
+        this.setState({ stations: stations });
+      })
   }
+
+  _renderCard() {
+    const { statInfo } = this.state
+    return (
+      <View style={styles.cardView}>
+        <View style={styles.card}>
+          <Text style={styles.cardName}>{statInfo.name}</Text>
+        </View>
+        <LinearGradient colors={['#55D0B4', '#51D0C4', '#4CD3D9']} style={styles.cardBtn}>
+          <Icon
+            name='plus'
+            type='feather'
+            color='#fff'
+            size={30} />
+        </LinearGradient>
+      </View>
+    )
+  }
+
+
 
   render() {
     return (
       this.state.stations ?
-        <MapView
-          region={this.state.region}
-          style={styles.container}
-        >
-          {this.state.stations.map((marker, index) => {
-            const coordinatesMap = marker.location.coordinates
-            const coordobj = {
-              latitude: coordinatesMap[1],
-              longitude: coordinatesMap[0]
-            }
-            return (
-              <MapView.Marker key={index}  coordinate={coordobj}>
-              </MapView.Marker>
-            );
-          })}
-        </MapView>
-        : 
-      <View style={styles.container}>
-        <Text>Wait!!!!</Text>
-      </View>
+        <View style={styles.container}>
+          <MapView
+            region={this.state.region}
+            style={styles.container}
+          >
+            {this.state.stations.map((marker, index) => {
+              const coordinatesMap = marker.location.coordinates
+              const coordobj = {
+                latitude: coordinatesMap[1],
+                longitude: coordinatesMap[0]
+              }
+              return (
+                <MapView.Marker key={index} onPress={() => {
+                  this.setState({
+                    isOpen: true,
+                    statInfo: marker,
+                    coords: coordobj,
+                  })
+                }} image={require('./assets/flash-marker.png')} coordinate={coordobj}>
+                </MapView.Marker>
+              );
+            })}
+          </MapView>
+          {this.state.isOpen ? this._renderCard() : null}
+        </View>
+        : <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+          <Text>WAITT!!!!!</Text>
+        </View>
     );
   }
 }
@@ -113,8 +122,79 @@ export default class Home extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+  },
+  cardView: {
+    position: "absolute",
+    bottom: 30,
+    left: 0,
+    right: 0,
+    padding: 20,
+  },
+  cardBtn: {
+    position: "absolute",
+    bottom: 80,
+    height: 50,
+    width: 50,
+    left: width - 95,
+    right: 0,
+    backgroundColor: 'red',
     alignItems: 'center',
     justifyContent: 'center',
+    borderRadius: 10,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 2,
+    elevation: 1,
+  },
+  card: {
+    backgroundColor: "#FFF",
+    height: CARD_HEIGHT,
+    width: '100%',
+    justifyContent: 'center',
+    overflow: "hidden",
+    borderRadius: 10,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 2,
+    elevation: 1,
+    padding: 20
+  },
+  cardName: {
+    fontSize: 20,
+    fontWeight: '700',
+    color: '#434343',
+    marginBottom: 5,
+  },
+  cardAddr: {
+    fontSize: 15,
+    color: '#C6C6C6',
+    marginBottom: 15,
+    marginRight: 35,
+  },
+  cardStatus: {
+    fontSize: 15,
+    color: '#4CD3D9'
+  },
+  markerWrap: {
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  marker: {
+    width: 8,
+    height: 8,
+    borderRadius: 4,
+    backgroundColor: "rgba(130,4,150, 0.9)",
+  },
+  ring: {
+    width: 24,
+    height: 24,
+    borderRadius: 12,
+    backgroundColor: "rgba(130,4,150, 0.3)",
+    position: "absolute",
+    borderWidth: 1,
+    borderColor: "rgba(130,4,150, 0.5)",
   },
 });
+
